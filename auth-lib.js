e7b2bd3aa4839caad2daee8bc457f384bac666bf
Bekzod Abakulov <bek.abakulov@gmail.com>
2019-03-14 14:02:28 +0500

    Fixed users and createUser functions

--- auth-lib.js
+++ auth-lib.js
@@ -1,8 +1,34 @@
-function createUser() {};
+var allUsers = [
+	{nickname: "admin", password: "1234", groups: ["admin", "manager", "basic"]},
+	{nickname: "sobakajozhec", password: "ekh228", groups: ["basic", "manager"]},
+	{nickname: "patriot007", password: "russiaFTW", groups: ["basic"]}
+];
 
-function deleteUser() {};
+var allRights = ["manage content", "play games", "delete users", "view site"];
 
-function users() {};
+var allGroups = {
+	"admin": [rights[2]],
+	"manager": [rights[0]],
+	"basic": [rights[1], rights[3]]
+}
+
+function createUser(nick, pass) {
+	allUsers.push({nickname: nick, password: pass, groups: ["basic"]});
+
+	return allUsers[allUsers.length - 1];
+};
+
+function deleteUser(nick) {
+	for (var i = 0; i < allUsers.length; i++) {
+		if (Object.values(allUsers[i]).indexOf(nick) > -1) {
+			allUsers.splice(i, 1);
+		}
+	}
+};
+
+function users() {
+	return allUsers;
+};
 
 function createGroup() {};
 
